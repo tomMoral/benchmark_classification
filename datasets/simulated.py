dd17ff5fd2796b6eb652eca430d0e677bd450953
UR4NVS <delta.cosmos22@gmail.com>
2023-06-09 13:44:07 +0200

    Added seed and random_state to simulated

--- datasets/simulated.py
+++ datasets/simulated.py
@@ -6,6 +6,7 @@ from benchopt import BaseDataset, safe_import_context
 # - getting requirements info when all dependencies are not installed.
 with safe_import_context() as import_ctx:
     from benchopt.datasets import make_correlated_data
+    import numpy as np
 
 
 # All datasets must be named `Dataset` and inherit from `BaseDataset`
@@ -22,6 +23,7 @@ class Dataset(BaseDataset):
             (1000, 500),
             (5000, 200),
         ],
+        'seed': [42]
     }
 
     def get_data(self):
@@ -30,7 +32,10 @@ class Dataset(BaseDataset):
         # API to pass data. It is customizable for each benchmark.
 
         # Generate pseudorandom data using `numpy`.
-        X, y, _ = make_correlated_data(self.n_samples, self.n_features)
+        rng = np.random.RandomState(self.seed)
+        X, y, _ = make_correlated_data(
+            self.n_samples, self.n_features, random_state=rng
+        )
         y = y > 0
 
         cat_indicator = [False]*X.shape[1]
