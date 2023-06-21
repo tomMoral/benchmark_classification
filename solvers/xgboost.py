1e566b1d4b6aa2510727f5e28e6e5c0533a061a3
tommoral <thomas.moreau.2010@gmail.com>
2023-06-21 16:43:54 +0200

    FIX parameters in histgradboosting+FIX end of lines

--- solvers/xgboost.py
+++ solvers/xgboost.py
@@ -16,9 +16,15 @@ class Solver(OSolver):
         return XGBClassifier()
 
     def sample_parameters(self, trial):
-        n_estimators = trial.suggest_int("n_estimators", 10, 2000, step=10)
-        max_depth = trial.suggest_int("max_depth", 1, 11, step=1)
+        n_estimators = trial.suggest_int("max_iter", 10, 2000, step=10)
+        l_rate = trial.suggest_float("learning_rate", 1e-2, 1, log=True)
+        max_leaves = trial.suggest_int("max_leaf_nodes", 3, 300, log=True)
+        min_child_weight = trial.suggest_int(
+            "min_samples_leaf", 1, 300, log=True
+        )
         return dict(
             n_estimators=n_estimators,
-            max_depth=max_depth,
+            learning_rate=l_rate,
+            max_leaves=max_leaves,
+            min_child_weight=min_child_weight
         )
