484fe1e6ce9dbdc18d43f737a960d3b248d1f7c8
SebastienMelo <sebastien.melo@polytechnique.edu>
2024-06-04 12:09:52 +0200

    adpated models to preprocessing

--- solvers/dummy.py
+++ solvers/dummy.py
@@ -3,6 +3,9 @@ from benchmark_utils.optuna_solver import OSolver
 
 with safe_import_context() as import_ctx:
     import optuna  # noqa: F401
+    from sklearn.pipeline import Pipeline
+    from sklearn.compose import ColumnTransformer
+    from sklearn.preprocessing import OneHotEncoder as OHE
     from sklearn.dummy import DummyClassifier
 
 
@@ -12,7 +15,19 @@ class Solver(OSolver):
     requirements = ["pip:optuna"]
 
     def get_model(self):
-        return DummyClassifier()
+        size = self.X_train.shape[1]
+        preprocessor = ColumnTransformer(
+            [
+                ("one_hot", OHE(
+                        categories="auto", handle_unknown="ignore",
+                    ), [i for i in range(size) if self.cat_ind[i]]),
+                ("numerical", "passthrough",
+                 [i for i in range(size) if not self.cat_ind[i]],)
+            ]
+        )
+        return Pipeline(steps= [("preprocessor", preprocessor),
+                                  ("model", DummyClassifier())])
+
 
     def sample_parameters(self, trial):
         strategy = trial.suggest_categorical("strategy", ["most_frequent", "prior", "stratified", "uniform"])
