c0528eaccbb6fb67a3048da6b123a7f1eb335312
ceelestin <celest1.eve@gmail.com>
2024-06-05 10:43:44 +0200

    adapted openml-datasets

--- datasets/datasets_openml.py
+++ datasets/datasets_openml.py
@@ -6,29 +6,29 @@ with safe_import_context() as import_ctx:
 
 
 DATASETS = {
-        #     'bank-marketing': 44126,
-        #     'california': 44090,
-        #     'compass': 44162,
-        #     'covertype': 44121,
-        #     'covertype_categorical': 44159,
-        #     'credit': 44089,
-        #     'electricity': 44120,
-        #     'electricity_categorical': 44156,
-        #     'eyemovements': 44130,
-        #     'eyemovements_categorical': 44156,
-        #     'higgs': 44129,
-        #     'house_16H': 44123,
-        #     'jannis': 44131,
-        #     'KDDCup06_upselling': 44186,
-        #     'kdd_ipums_la_97-small': 44124,
-        #     'magictelescope': 44125,
-        #     'MiniBooNE': 44128,
-        #     'phoneme': 44127,
-        #     'pol': 44122,
-        #     'rl': 44160,
-        #     'road_safety': 44161,
-        #     'wine': 44091,
-        #      42803,
+        # 44126,  # bank-marketing
+        # 44090,  # california
+        # 44162,  # compass
+        # 44121,  # covertype
+        # 44159,  # covertype_categorical
+        # 44089,  # credit
+        # 44120,  # electricity
+        # 44156,  # electricity_categorical
+        # 44130,  # eyemovements
+        # 44156,  # eyemovements_categorical
+        # 44129,  # higgs
+        # 44123,  # house_16H
+        # 44131,  # jannis
+        # 44186,  # KDDCup06_upselling
+        # 44124,  # kdd_ipums_la_97-small
+        # 44125,  # magictelescope
+        # 44128,  # MiniBooNE
+        # 44127,  # phoneme
+        # 44122,  # pol
+        # 44160,  # rl
+        # 44161,  # road_safety
+        # 44091,  # wine
+        # 42803,
         151,
 }
 
